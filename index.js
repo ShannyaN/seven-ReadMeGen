23944ae0533622a6ddf6d4aa8076f1dfcae11f99
Shannya <niveyro101@gmail.com>
2023-03-02 00:53:08 -0500

    prompt complete

--- index.js
+++ index.js
@@ -6,7 +6,37 @@ inquirer
         {
             type: "input",
             message: "File title: ",
-            name: "fileName"
+            name: "fileTitle"
+        },
+        {
+            type:"input",
+            message: "What is the description of this repo ",
+            name: "description"
+        },
+        {
+            type:"input",
+            message: "What kinds of installation do you need to access the file? ",
+            name: "installation"
+        },
+        {
+            type:"input",
+            message: "Provide and example of usage. ",
+            name: "usage"
+        },
+        {
+            type:"input",
+            message: "Are you open to contributions? ",
+            name: "contributions"
+        },
+        {
+            type:"input",
+            message: "What kind of tests can be done with this repo? ",
+            name: "tests"
+        },
+        {
+            type:"input",
+            message: "Any questions? ",
+            name: "questions"
         }
     ])
     .then ((response)=> {
@@ -20,3 +50,4 @@ inquirer
     //    fs.writeFile(README.md, ${response}, (err) =>
      //   err ? console.error(err) : console.log('Response logged!'));
     //)
+//THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
